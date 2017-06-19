fe23bd14efb45f4f3e603a0a288a32d14a0e8c2f
DapengFeng <1052514473@qq.com>
2017-06-19 15:08:07 +0800

    Update Problem Set 5

--- Problem Sets/Problem Set 5/student.cu
+++ Problem Sets/Problem Set 5/student.cu
@@ -27,17 +27,52 @@
 
 #include "utils.h"
 
+const int K = 128;
+const int numStreams = 2;
+
 __global__
 void yourHisto(const unsigned int* const vals, //INPUT
                unsigned int* const histo,      //OUPUT
-               int numVals)
+               unsigned int numVals)
 {
   //TODO fill in this kernel to calculate the histogram
   //as quickly as possible
-
+  __shared__ unsigned int sdata[K];
+  
+  unsigned int myId = threadIdx.x + blockIdx.x * blockDim.x;
+  int id = threadIdx.x;
+  
+  if (myId >= numVals) {
+    return;
+  }
+  
+  sdata[id] = vals[myId];
+  __syncthreads();
+  
+  atomicAdd(&histo[sdata[id]], 1);
   //Although we provide only one kernel skeleton,
   //feel free to use more if it will help you
   //write faster code
+}
+
+__global__
+void mergeHisto(unsigned int* histo1,
+                unsigned int* histo2,
+                unsigned int* const histo
+                )
+{
+  __shared__ unsigned int sdata1[K];
+  __shared__ unsigned int sdata2[K];
+  
+  unsigned int myId = threadIdx.x + blockIdx.x * blockDim.x;
+  int id = threadIdx.x;
+  
+  sdata1[id] = histo1[myId];
+  sdata2[id] = histo2[myId];
+  __syncthreads();
+  
+  sdata1[id] += sdata2[id];
+  histo[myId] = sdata1[id];
 }
 
 void computeHistogram(const unsigned int* const d_vals, //INPUT
@@ -46,9 +81,41 @@ void computeHistogram(const unsigned int* const d_vals, //INPUT
                       const unsigned int numElems)
 {
   //TODO Launch the yourHisto kernel
+  cudaStream_t s1,s2;
+  const int numElemsPerStream =  numElems / numStreams;
+  dim3 threads(K);
+  dim3 blocks(numElemsPerStream / K);
+  
+  unsigned int *d_vals1, *d_vals2;
+  //unsigned int *d_histo1, *d_histo2;
+  
+  checkCudaErrors(cudaMalloc(&d_vals1, numElemsPerStream * sizeof(unsigned int)));
+  //checkCudaErrors(cudaMalloc(&d_histo1, numElemsPerStream * sizeof(unsigned int)));
+  checkCudaErrors(cudaMalloc(&d_vals2, numElemsPerStream * sizeof(unsigned int)));
+  //checkCudaErrors(cudaMalloc(&d_histo2, numElemsPerStream * sizeof(unsigned int)));
+  
+  checkCudaErrors(cudaStreamCreate(&s1));
+  checkCudaErrors(cudaStreamCreate(&s2));
+  
+  checkCudaErrors(cudaMemcpyAsync(d_vals1, d_vals, numElemsPerStream * sizeof(unsigned int), cudaMemcpyDeviceToDevice, s1));
+  yourHisto<<<blocks,threads,0,s1>>>(d_vals1, d_histo, numElemsPerStream);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+  
+  checkCudaErrors(cudaMemcpyAsync(d_vals2, d_vals+numElemsPerStream, numElemsPerStream * sizeof(unsigned int), cudaMemcpyDeviceToDevice, s2));
+  yourHisto<<<blocks,threads,0,s2>>>(d_vals2, d_histo, numElemsPerStream);
+  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
 
   //if you want to use/launch more than one kernel,
   //feel free
-
-  cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+  //mergeHisto<<<numBins / K, K>>>(d_histo1, d_histo2,d_histo);
+  //cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
+  //yourHisto<<<numElems/K, threads>>>(d_vals,d_histo, numElems);
+  
+  checkCudaErrors(cudaStreamDestroy(s1));
+  checkCudaErrors(cudaStreamDestroy(s2));
+  
+  cudaFree(d_vals1);
+  cudaFree(d_vals2);
+  //cudaFree(d_histo1);
+  //cudaFree(d_histo2);
 }
